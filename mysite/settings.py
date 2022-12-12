cb5cd75588ecb415587142385e8b1686865b366e
BryamVC <bryamvega7@gmail.com>
2022-12-11 20:44:13 -0500

    config db

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'CtyTwOgHCfSU1DI1l3Nz',
+        'HOST': 'containers-us-west-143.railway.app',
+        'PORT': '5764',
     }
 }
 
