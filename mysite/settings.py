4e663d35b44cb66f7614b650cd097e906afe0761
BryamVC <bryamvega7@gmail.com>
2022-12-11 22:49:09 -0500

    config db

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'mysite/staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'mysite/static'),)
 
 # Default primary key field type
