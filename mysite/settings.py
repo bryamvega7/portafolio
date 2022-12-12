b0ec350d9e01fa9b595cd8e3efdb914ac899f124
BryamVC <bryamvega7@gmail.com>
2022-12-11 23:02:50 -0500

    config db

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/' 
-STATICFILES_DIRS = [BASE_DIR/'static'] 
+STATICFILES_DIRS = [BASE_DIR/'mysite/static'] 
 STATIC_ROOT = BASE_DIR / 'staticfiles' 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
