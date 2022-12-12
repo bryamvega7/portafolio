114a7e012fef8e725ef356fab99f0e0c20ddef55
BryamVC <bryamvega7@gmail.com>
2022-12-11 22:18:44 -0500

    config db

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,8 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-LOGIN_URL = 'login'
+MEDIA_ROOT = BASE_DIR / "static"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'mysite/static'),)
+MEDIA_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
